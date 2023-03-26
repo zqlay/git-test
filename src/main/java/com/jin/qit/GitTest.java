413edb912b00cc3b9d46b18c29eff5a29f62dce5
zhouqin <3474823183@qq.com>
2023-03-26 17:02:16 +0800

    second commit

--- src/main/java/com/jin/qit/GitTest.java
+++ src/main/java/com/jin/qit/GitTest.java
@@ -3,5 +3,6 @@ package com.jin.qit;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git1");
+        System.out.println("hello git2");
     }
 }
