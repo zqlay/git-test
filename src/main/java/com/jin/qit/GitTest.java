ba32701385228e49c19c8468ad997614d20b8a1f
zhouqin <3474823183@qq.com>
2023-03-26 17:21:06 +0800

    hot-fix commit1

--- src/main/java/com/jin/qit/GitTest.java
+++ src/main/java/com/jin/qit/GitTest.java
@@ -5,5 +5,7 @@ public class GitTest {
         System.out.println("hello git1");
         System.out.println("hello git2");
         System.out.println("hello git3");
+        System.out.println("hello git4");
+
     }
 }
